4a69a127cc9a3f7b9cf3e209d6f916a93713fd5c
Rolf Staflin <rolf.staflin@tacton.com>
2019-12-03 20:00:45 +0100

    Day 03 done.

--- lib/Day03.dart
+++ lib/Day03.dart
@@ -8,6 +8,28 @@ class Point {
   Point(this.x, this.y);
   Point.origin() { x = 0;  y = 0; }
 
+  Point operator +(Point other) {
+    return Point(x + other.x, y + other.y);
+  }
+
+  bool isInside(Rect r) {
+    return x >= r.left && x <= r.right && y >= r.bottom && y <= r.top;
+  }
+
+  int manhattanDistanceFromOrigin() {
+    return x.abs() + y.abs();
+  }
+
+
+  @override
+  bool operator ==(other) {
+    if (other.runtimeType == Point) {
+      var p = other as Point;
+      return x == p.x && y == p.y;
+    }
+    return false;
+  }
+
   @override String toString() { return '($x, $y)'; }
 }
 
@@ -23,6 +45,51 @@ class Rect {
   int height() { return top - bottom; }
 
   @override String toString() {return 'Rect{($left, $bottom), ($right, $top)}';}
+
+  void grow(int i) {
+    bottom -= i;
+    left -= i;
+    top += i;
+    right += i;
+  }
+}
+
+class Grid {
+  Rect bounds;
+  List<List<String>> grid;
+
+  Grid(Rect bounds) {
+    this.bounds = bounds;
+    grid = List<List<String>>.generate(bounds.height() + 1, (i) =>
+    List<String>.generate(bounds.width() + 1, (j) => "."));
+  }
+
+  void set(Point p, String mark) {
+    if (p.isInside(bounds)) {
+      var y = p.y - bounds.bottom;
+      var x = p.x - bounds.left;
+      grid[y][x] = mark;
+    }
+  }
+
+  String get(Point p) {
+    if (p.isInside(bounds)) {
+      var y = p.y - bounds.bottom;
+      var x = p.x - bounds.left;
+      return grid[y][x];
+    }
+    return "";
+  }
+
+  draw() {
+    for (var row in grid.reversed) {
+      var line = "";
+      for (var col in row) {
+        line += col;
+      }
+      print(line);
+    }
+  }
 }
 
 Rect getBounds(String content) {
@@ -61,6 +128,50 @@ Rect getBounds(String content) {
   return bounds;
 }
 
+//---------------------
+
+List<Point> crossings = List();
+
+void drawPath(String path, Grid grid, List<Point> linePoints) {
+  var directions = path.split(",");
+
+  Point p = Point.origin();
+  for (var dir in directions) {
+    var distance = int.parse(dir.substring(1, dir.length));
+    switch (dir[0]) {
+      case "U":
+        p = move(p, Point(0, 1), "|", distance, grid, linePoints);
+        break;
+      case "L":
+        p = move(p, Point(-1, 0), "-", distance, grid, linePoints);
+        break;
+      case "D":
+        p = move(p, Point(0, -1), "|", distance, grid, linePoints);
+        break;
+      case "R":
+        p = move(p, Point(1, 0), "-", distance, grid, linePoints);
+        break;
+    }
+    grid.set(p, "+");
+  }
+}
+
+
+Point move(Point start, Point delta, String mark, int distance, Grid grid, List<Point> points) {
+  Point p = start;
+  for (int i = 0; i < distance; i++) {
+    p += delta;
+    points.add(p);
+    var gridValue = grid.get(p);
+    if (gridValue == "-" || gridValue == "|") {
+      crossings.add(p);
+      grid.set(p, "X");
+    } else {
+      grid.set(p, mark);
+    }
+  }
+  return p;
+}
 
 Future<void> day03() async {
   var file = File('input/Day03.txt');
@@ -68,18 +179,30 @@ Future<void> day03() async {
   if (await file.exists()) {
     var contents = await file.readAsLines();
     var bounds = getBounds(contents[0]);
+    bounds.grow(2);
 
-    final size = 5;
-    final grid = List<List<String>>.generate(bounds.height(),
-            (i) => List<String>.generate(bounds.width(), (j) => (i * size + j).toString()));
+    var grid = Grid(bounds);
+    grid.set(Point.origin(), "O");
 
-    print(bounds);
-    print(grid);
+    var firstLine = List<Point>();
+    drawPath(contents[0], grid, firstLine);
+    crossings.clear();
+    var secondLine = List<Point>();
+    drawPath(contents[1], grid, secondLine);
 
-    print(grid[2][4]);
-    var directions = contents[1].split(",");
-    print(directions);
+    // Filter out crossings where the second line crossed itself
+    crossings = crossings.where((crossing) => (firstLine.indexWhere((p) => p == crossing) > -1)).toList();
+    crossings.sort((a, b) => a.manhattanDistanceFromOrigin().compareTo(b.manhattanDistanceFromOrigin()));
+    print("Part 1: ${crossings[0].manhattanDistanceFromOrigin()}");
+
+    var closest = firstLine.length + secondLine.length;
+    for (var crossing in crossings) {
+      var combinedSteps = firstLine.indexWhere((p) => p == crossing) + 1 +
+                          secondLine.indexWhere((p) => p == crossing) + 1;
+      if (combinedSteps < closest) {
+        closest = combinedSteps;
+      }
+    }
+    print("Part 2: $closest");
   }
 }
-
-

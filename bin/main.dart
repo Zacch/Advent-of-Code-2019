746f266ceea47abb7e70e9de6fb360275afa1a05
Rolf Staflin <rex@piro.se>
2019-12-05 07:52:20 +0100

    Day 05 started

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,6 @@
 
-import 'package:Advent_of_Code_2019/Day04.dart';
+import 'package:Advent_of_Code_2019/Day05.dart';
 
 main(List<String> arguments) async {
-  await day04();
+  await day05();
 }
---
+++ lib/Day05.dart
@@ -0,0 +1,139 @@
+import 'dart:io';
+
+Future<void> day05() async {
+  var file = File('input/Day05.txt');
+
+  if (await file.exists()) {
+    var contents = await file.readAsString();
+    var parts = contents.split(',');
+    var startingMemory = parts.map((s) => int.parse(s)).toList();
+    var computer = IntcodeComputer(startingMemory, [1]);
+    computer.execute();
+    print('Part 1: ${computer.output}');
+  }
+}
+
+class IntcodeComputer {
+  List<int> memory;
+  int ip;
+  List<int> input;
+  List<int> output;
+  bool isRunning;
+
+  IntcodeComputer(this.memory, this.input);
+
+  List<int> execute() {
+    ip = 0;
+    isRunning = true;
+    while (isRunning) {
+      var instruction = getNextInstruction();
+      instruction.execute(this);
+    }
+    return output;
+  }
+
+  Instruction getNextInstruction() {
+    var value = memory[ip];
+    var opcode = value % 100;
+    Instruction instruction;
+    switch (opcode) {
+      case 1:
+        instruction = Add();
+        break;
+      case 2:
+        instruction = Multiply();
+        break;
+      case 3:
+        instruction = Input();
+        break;
+      case 4:
+        instruction = Output();
+        break;
+      case 99:
+        instruction = Halt();
+        break;
+      default:
+        print("ERROR!");
+    }
+    int rest = value ~/ 100;
+    while (rest > 0) {
+      instruction.modes.add(rest % 10);
+      rest ~/= 10;
+    }
+    while (instruction.modes.length < 3) {
+      instruction.modes.add(0);
+    }
+    return instruction;
+  }
+
+  setMemory(int position, int value) {
+    memory[position] = value;
+  }
+}
+
+abstract class Instruction {
+  var modes = List<int>();
+  var parameters = List<int>();
+
+  void execute(IntcodeComputer state);
+
+  // Loads parameters into the instruction and increments the instruction pointer
+  void load(IntcodeComputer state, int parameterCount) {
+    state.ip++;
+    for(var i = 0; i < parameterCount; i++) {
+      var parameter = state.memory[state.ip + i];
+      switch (modes[i]) {
+        case 0:
+          parameters.add(state.memory[parameter]);
+          break;
+        case 1:
+          parameters.add(parameter);
+          break;
+      }
+    }
+    state.ip += parameterCount;
+  }
+}
+
+class Add extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    load(state, 3);
+    state.setMemory(parameters[2], parameters[0] + parameters[1]);
+  }
+}
+
+class Multiply extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    load(state, 3);
+    state.setMemory(parameters[2], parameters[0] * parameters[1]);
+  }
+}
+
+class Input extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    // We want the immediate argument here
+    modes[0] = 1;
+    load(state, 1);
+    state.setMemory(parameters[0], state.input.removeAt(0));
+  }
+}
+
+class Output extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    load(state, 1);
+    state.output.add(state.memory[parameters[0]]);
+  }
+}
+
+class Halt extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+  load(state, 0);
+  state.isRunning = false;
+  }
+}
+

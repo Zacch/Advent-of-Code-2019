3b8a457e9904606bfb2d2b91f8ec8e377de93996
Rolf Staflin <rolf.staflin@tacton.com>
2019-12-05 18:01:31 +0100

    Day 05 solved

--- lib/Day05.dart
+++ lib/Day05.dart
@@ -10,6 +10,12 @@ Future<void> day05() async {
     var computer = IntcodeComputer(startingMemory, [1]);
     computer.execute();
     print('Part 1: ${computer.output.last}');
+
+    // Start over with input 5
+    startingMemory = parts.map((s) => int.parse(s)).toList();
+    computer = IntcodeComputer(startingMemory, [5]);
+    computer.execute();
+    print('Part 2: ${computer.output.first}');
   }
 }
 
@@ -48,6 +54,18 @@ class IntcodeComputer {
         break;
       case 4:
         instruction = Output();
+        break;
+      case 5:
+        instruction = JumpIfTrue();
+        break;
+      case 6:
+        instruction = JumpIfFalse();
+        break;
+      case 7:
+        instruction = LessThan();
+        break;
+      case 8:
+        instruction = Equal();
         break;
       case 99:
         instruction = Halt();
@@ -134,8 +152,46 @@ class Output extends Instruction {
 class Halt extends Instruction {
   @override
   void execute(IntcodeComputer state) {
-  load(state, 0);
-  state.isRunning = false;
+    load(state, 0);
+    state.isRunning = false;
+  }
+}
+
+class JumpIfTrue extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    load(state, 2);
+    if (parameters[0] != 0) {
+      state.ip = parameters[1];
+    }
+  }
+}
+
+class JumpIfFalse extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    load(state, 2);
+    if (parameters[0] == 0) {
+      state.ip = parameters[1];
+    }
+  }
+}
+
+class LessThan extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    modes[2] = 1;
+    load(state, 3);
+    state.setMemory(parameters[2], (parameters[0] < parameters[1]) ? 1 : 0);
+  }
+}
+
+class Equal extends Instruction {
+  @override
+  void execute(IntcodeComputer state) {
+    modes[2] = 1;
+    load(state, 3);
+    state.setMemory(parameters[2], (parameters[0] == parameters[1]) ? 1 : 0);
   }
 }
 

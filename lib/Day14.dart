0974cd1d1823479340a0ed7aaf6e077f072b2ef9
Rolf Staflin <rex@piro.se>
2019-12-14 22:10:57 +0100

    Day 14 solved

--- lib/Day14.dart
+++ lib/Day14.dart
@@ -1,26 +1,5 @@
 import 'dart:io';
 
-class Reagent {
-  int amount = 0;
-  String name;
-
-  Reagent(this.amount, this.name);
-
-  static Reagent parse(String string) {
-    var parts = string.split(' ').toList();
-    if (parts.length < 2) { return Reagent(0, string); }
-    return Reagent(int.parse(parts[0]), parts[1]);
-  }
-
-  @override String toString() { return '$amount $name'; }
-
-  @override bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Reagent && runtimeType == other.runtimeType && name == other.name;
-
-  @override int get hashCode => name.hashCode;
-}
-
 const ONE_TRILLION = 1000000000000;
 
 var formulae = Map<Reagent, List<Reagent>>();
@@ -39,48 +18,21 @@ Future<void> day14() async {
       var ingredients = parts[0].split(', ').map((i) => Reagent.parse(i)).toList();
       formulae[result] = ingredients;
     });
-//    formulae.forEach((k,v) {
-//      print('$v => $k');
-//    });
 
     buildOneFuel();
     print('Part 1: $oreUsed');
 
-//    int fuelProduced = 1;
-//    var history = Map<String, int>();
-//    var oreUsedHistory = [0, oreUsed];
-//    history[leftovers.toString()] = fuelProduced;
-//    print('fuelProduced: $fuelProduced, oreUsed: $oreUsed leftovers: $leftovers');
-//
-//    while (fuelProduced < 300) {
-//      buildOneFuel();
-//      fuelProduced++;
-//      oreUsedHistory.add(oreUsed);
-//      var leftOverString = leftovers.toString();
-//      print('fuelProduced: $fuelProduced, oreUsed: $oreUsed leftovers: $leftOverString');
-//      if (history.containsKey(leftOverString)) {
-//        var loopStart = history[leftOverString];
-//        print('Matches iteration $loopStart!');
-//        var loopLength = fuelProduced - loopStart;
-//        var loopOre = oreUsed - oreUsedHistory[loopStart];
-//        print('loopLength: $loopLength, loopOre: $loopOre');
-//
-//        var loops = ONE_TRILLION ~/ loopOre;
-//        var oreLeft = ONE_TRILLION - loops * loopOre;
-//        var part2 = loops * loopLength;
-//        print('loops: $loops, oreLeft: $oreLeft, total fuel: $part2');
-//
-//        oreUsed = 0;
-//        while (oreUsed < oreLeft)  {
-//          part2++;
-//          buildOneFuel();
-//        }
-//        print('Part 2: $part2');
-//        print('Part 2: 82892753');
-//        //break;
-//      }
-//    }
-//
+    int fuelProduced = 1;
+
+    // When in doubt, use brute force...  O.o
+    while (oreUsed < ONE_TRILLION) {
+      buildOneFuel();
+      fuelProduced++;
+      if (fuelProduced % 100000 == 0) {
+        print('Please wait :). We still have ${ONE_TRILLION - oreUsed} ore to use');
+      }
+    }
+    print('Part 2: ${fuelProduced - 1}');
   }
 }
 
@@ -90,23 +42,14 @@ void buildOneFuel() {
   for (var reagent in formulae[Reagent(1, "FUEL")]) {
     needed[reagent.name] = reagent.amount;
   }
-  var totalNeeded = Map<String, int>();  // ------ Not used!
   while (needed.isNotEmpty) {
- //   print('ore $oreUsed + $needed.   Leftovers $leftovers    Total needed: $totalNeeded');
     var chemical = needed.keys.first;
-    var amountNeeded = needed[chemical];
-    if (leftovers.containsKey(chemical)) {
-      var leftover = leftovers[chemical];
-      if (amountNeeded < leftover) {
-        leftovers[chemical] -= amountNeeded;
-        continue;
-      }
-      amountNeeded -= leftover;
-      leftovers.remove(chemical);
-      if (amountNeeded == 0) { continue; }
-    }
+    var amountNeeded = takeFromLeftovers(chemical, needed[chemical]);
     needed.remove(chemical);
-    var resultingAmount = formulae.keys.firstWhere((k) => k.name == chemical).amount;
+    if (amountNeeded == 0) { continue; }
+    var resultingAmount = formulae.keys
+        .firstWhere((k) => k.name == chemical)
+        .amount;
     var batches = (amountNeeded / resultingAmount).ceil();
     var extra = batches * resultingAmount - amountNeeded;
     if (extra > 0) {
@@ -115,21 +58,13 @@ void buildOneFuel() {
     var formula = formulae[Reagent(amountNeeded, chemical)];
     for (var reagent in formula) {
       if (reagent.name == "ORE") {
-        totalNeeded[chemical] = (totalNeeded[chemical] ?? 0) + amountNeeded;
         oreUsed += batches * reagent.amount;
         continue;
       }
 
-      var reagentAmountNeeded = reagent.amount * batches;
-      if (leftovers.containsKey(reagent.name)) {
-        var leftover = leftovers[reagent.name];
-        if (reagentAmountNeeded < leftover) {
-          leftovers[reagent.name] -= reagentAmountNeeded;
-          continue;
-        }
-        reagentAmountNeeded -= leftover;
-        leftovers.remove(reagent.name);
-        if (reagentAmountNeeded == 0) { continue; }
+      var reagentAmountNeeded = takeFromLeftovers(reagent.name, reagent.amount * batches);
+      if (reagentAmountNeeded == 0) {
+        continue;
       }
 
       if (needed.containsKey(reagent.name)) {
@@ -139,4 +74,38 @@ void buildOneFuel() {
       }
     }
   }
+}
+
+int takeFromLeftovers(String chemical, int reagentAmountNeeded) {
+  if (leftovers.containsKey(chemical)) {
+    var leftover = leftovers[chemical];
+    if (reagentAmountNeeded < leftover) {
+      leftovers[chemical] -= reagentAmountNeeded;
+      return 0;
+    }
+    reagentAmountNeeded -= leftover;
+    leftovers.remove(chemical);
+  }
+  return reagentAmountNeeded;
+}
+
+class Reagent {
+  int amount = 0;
+  String name;
+
+  Reagent(this.amount, this.name);
+
+  static Reagent parse(String string) {
+    var parts = string.split(' ').toList();
+    if (parts.length < 2) { return Reagent(0, string); }
+    return Reagent(int.parse(parts[0]), parts[1]);
+  }
+
+  @override String toString() { return '$amount $name'; }
+
+  @override bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Reagent && runtimeType == other.runtimeType && name == other.name;
+
+  @override int get hashCode => name.hashCode;
 }

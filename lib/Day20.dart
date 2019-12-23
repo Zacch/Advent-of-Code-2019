54c78c158a7b0645238ab637b6f9db54d857b11b
Rolf Staflin <rex@piro.se>
2019-12-24 00:46:49 +0100

    Day 20 solved.

--- lib/Day20.dart
+++ lib/Day20.dart
@@ -11,6 +11,9 @@ var Z = 'Z'.codeUnitAt(0);
 var VOID = ' '.codeUnitAt(0);
 
 Grid grid;
+var height = 0;
+var width = 0;
+var teleports = Map<String, List<Point>>();
 var connections = Map<Point, Point>();
 
 Future<void> day20() async {
@@ -18,9 +21,9 @@ Future<void> day20() async {
 
   if (await file.exists()) {
     var contents = await file.readAsLines();
-    var h = contents.length;
-    var w = contents[0].length;
-    grid = Grid(Rect(0, 0, h - 1, w - 1));
+    height = contents.length;
+    width = contents[0].length;
+    grid = Grid(Rect(0, 0, height - 1, width - 1));
     var p = Point(0, 0);
     for (var line in contents) {
       line.codeUnits.forEach((c) {
@@ -31,10 +34,9 @@ Future<void> day20() async {
       p.x = 0;
     }
 
-    var teleports = Map<String, List<Point>>();
     var ignoreLetterAt = List<Point>();
-    for (p.y = 0; p.y < h; p.y++) {
-      for (p.x = 0; p.x < w; p.x++) {
+    for (p.y = 0; p.y < height; p.y++) {
+      for (p.x = 0; p.x < width; p.x++) {
         var c = grid.getChar(p);
         if (isLetter(c) && !ignoreLetterAt.contains(p)) {
           var p2 = p + Point(0, 1);
@@ -57,7 +59,6 @@ Future<void> day20() async {
           }
           ignoreLetterAt.add(p2);
           var teleportName = String.fromCharCodes([c, c2]);
-          print('Found ${teleportName} at $p');
           if (!teleports.containsKey(teleportName)) {
             teleports[teleportName] = List<Point>();
           }
@@ -69,19 +70,14 @@ Future<void> day20() async {
     var goal = teleports['ZZ'].first;
     teleports.remove('AA');
     teleports.remove('ZZ');
-    print(teleports);
-    print('Start $start, Goal $goal');
     for (var points in teleports.values) {
       connections[points[0]] = points[1];
       connections[points[1]] = points[0];
     }
-    print(connections);
-//    connections.keys.forEach((p) {grid.set(p, '*');});
-//    grid.set(start, 's');
-//    grid.set(goal, 'g');
 
     print('Part 1: ${shortestPath(start, goal)}');
-    grid.draw();
+    print('Part 2: ${shortestRecursivePath(start, goal)}');
+
   }
 }
 
@@ -99,17 +95,13 @@ int shortestPath(Point start, Point goal) {
       possibleMoves.add(connections[p]);
     }
     for (var move in possibleMoves) {
-      if (visited.contains(move)) {
-        continue;
-      }
-      if (!(grid.get(move) == '.')) {
+      if (visited.contains(move) || grid.get(move) != '.') {
         continue;
       }
 
       var next = List<Point>.from(current);
       next.add(move);
       if (move == goal) {
-        next.forEach((p) {grid.set(p, '_');});
         return (next.length - 1);
       }
       frontier.add(next);
@@ -119,6 +111,48 @@ int shortestPath(Point start, Point goal) {
   return -1;
 }
 
+
+int shortestRecursivePath(Point start2d, Point goal2d) {
+  print('Please wait – this will take a while...');
+  var start = Point3.from(start2d);
+  var goal = Point3.from(goal2d);
+  var frontier = Queue<List<Point3>>();
+  frontier.add([start]);
+  var visited = List<Point3>();
+  visited.add(start);
+
+  while (frontier.isNotEmpty) {
+    var current = frontier.removeFirst();
+    Point3 p = current.last;
+    var possibleMoves = [p + Point3.NORTH, p + Point3.SOUTH, p + Point3.WEST, p + Point3.EAST];
+    var p2 = p.toPoint();
+    if (connections.containsKey(p2)) {
+      if (isInnerWall(p)) {
+        possibleMoves.add(Point3(connections[p2].x, connections[p2].y, p.z + 1));
+      } else if (p.z > 0) {
+        possibleMoves.add(Point3(connections[p2].x, connections[p2].y, p.z - 1));
+      }
+    }
+
+    for (var move in possibleMoves) {
+      if (visited.contains(move) || grid.get(move.toPoint()) != '.') {
+        continue;
+      }
+      var next = List<Point3>.from(current);
+      next.add(move);
+      if (move == goal) {
+        return (next.length - 1);
+      }
+      frontier.add(next);
+      visited.add(move);
+    }
+  }
+  return -1;
+}
+
+bool isInnerWall(Point3 p) {
+  return p.x > 2 && p.y > 2 && p.x < width - 3 && p.y < height - 3;
+}
 
 bool isLetter(int c) {
   return c >= A && c <= Z;
--- lib/Point.dart
+++ lib/Point.dart
@@ -69,3 +69,42 @@ class Point {
 
   @override String toString() { return '($x, $y)'; }
 }
+
+class Point3 {
+  int x;
+  int y;
+  int z;
+
+  Point3(this.x, this.y, this.z);
+
+  Point3.from(Point p) { x = p.x; y = p.y; z = 0; }
+  Point3.origin() { x = 0; y = 0; z = 0; }
+
+  static final ORIGIN = Point3.origin();
+  static final UP = Point3(0, 0, 1);
+  static final DOWN = Point3(0, 0, -1);
+  static final NORTH = Point3(0, 1, 0);
+  static final SOUTH = Point3(0, -1, 0);
+  static final EAST = Point3(1, 0, 0);
+  static final WEST = Point3(-1, 0, 0);
+
+  Point3 copy() {
+    return Point3(x, y, z);
+  }
+
+  Point3 operator +(Point3 other) {
+    return Point3(x + other.x, y + other.y, z + other.z);
+  }
+
+  Point toPoint() { return Point(x, y); }
+
+  @override String toString() { return '($x, $y, $z)'; }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Point3 && runtimeType == other.runtimeType && x == other.x && y == other.y && z == other.z;
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode ^ z.hashCode;
+}

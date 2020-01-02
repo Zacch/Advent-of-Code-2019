6005180efb8e9331b928f219bd33485693a7cf55
Rolf Staflin <rex@piro.se>
2020-01-03 00:16:36 +0100

    Day 23 solved

--- lib/Day23.dart
+++ lib/Day23.dart
@@ -1,6 +1,8 @@
 
 import 'dart:io';
 
+import 'package:Advent_of_Code_2019/Point.dart';
+
 import 'IntcodeComputer.dart';
 import 'IntcodeInstruction.dart';
 
@@ -22,9 +24,11 @@ Future<void> day23() async {
       network.add(computer);
     }
 
-    var part1 = -1;
+    var natPacket = Point(-1, -1);
     var steps = 0;
-    while (part1 == -1) {
+    var natYvalues = Set<int>();
+    var part2Done = false;
+    while (!part2Done) {
       for (int i = 0; i < 50; i++) {
         var computer = network[i];
         if (computer.currentInstruction is Input && computer.input.isEmpty) {
@@ -32,21 +36,44 @@ Future<void> day23() async {
         }
         computer.step();
         if (computer.output.length == 3) {
-          print('Output from $i to ${computer.output[0]}: X = ${computer.output[1]}, Y = ${computer.output[2]} after $steps steps');
           var destination = computer.output[0];
           if (destination == 255) {
-            part1 = computer.output[2];
+            if (natPacket.y == -1) {
+              print('Part 1: ${computer.output[2]}');
+            }
+            natPacket.x = computer.output[1];
+            natPacket.y = computer.output[2];
           } else {
             network[destination].input.add(computer.output[1]);
             network[destination].input.add(computer.output[2]);
           }
           computer.output.clear();
         }
+        if (isIdle(network)) {
+          network[0].input.add(natPacket.x);
+          network[0].input.add(natPacket.y);
+          if (natYvalues.contains(natPacket.y)) {
+            print('Part 2: ${natPacket.y}');
+            part2Done = true;
+          }
+          natYvalues.add(natPacket.y);
+        }
       }
       if (++steps % 1000000 == 0) {
         print('${steps ~/ 1000000} million steps');
       }
     }
-    print('Part 1: $part1');
   }
+}
+
+bool isIdle(List<IntcodeComputer> network) {
+  for (var computer in network) {
+    if (computer.input.isNotEmpty) {
+      return false;
+    }
+    if (computer.ip < 73 || computer.ip > 87) {
+      return false;
+    }
+  }
+  return true;
 }
